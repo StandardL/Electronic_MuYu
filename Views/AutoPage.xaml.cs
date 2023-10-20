e8fe83e0c8c9e17e2de2232b1a9ef7df937ab2a8
StandardL <821356557@qq.com>
2023-10-20 20:42:42 +0800

    异步实现电子木鱼的自动敲击。

--- Views/AutoPage.xaml.cs
+++ Views/AutoPage.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using Electronic_MuYu.ViewModels;
-
 using Microsoft.UI.Xaml.Controls;
 
 namespace Electronic_MuYu.Views;
 
 public sealed partial class AutoPage : Page
 {
+    private int counter;
+    private string count;
+
+    private bool isRunning;
     public AutoViewModel ViewModel
     {
         get;
@@ -15,16 +18,38 @@ public sealed partial class AutoPage : Page
     {
         ViewModel = App.GetService<AutoViewModel>();
         InitializeComponent();
+
+
+        counter = 0;
+        count = counter.ToString();
     }
 
-    private void Auto_Knock_Start_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-    {
 
+    private async Task UpdateCounterAsync()
+    {
+        while (isRunning)
+        {
+            counter++;
+            count = counter.ToString();
+            GongDe_Counter.Text = count;
+            await Task.Delay(1250); // 1秒的延迟
+        }
+    }
+    private async void Auto_Knock_Start_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        if (!isRunning)
+        {
+            isRunning = true;
+            await UpdateCounterAsync();
+        }
     }
 
     private void Auto_Knock_Stop_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-
+        if (isRunning)
+        {
+            isRunning = false;
+        }
     }
 
     private void Read_Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
